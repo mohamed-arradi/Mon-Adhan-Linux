162830be962fc2e821754b16656bd3051720b912
Mohamed <arradi.mohamed@gmail.com>
2022-01-20 21:51:30 +0400

    reduce json file

--- TermiAdhan/erase-json.py
+++ TermiAdhan/erase-json.py
@@ -4,8 +4,9 @@ with open('./resources/citydatas.json') as data_file:
     data = json.load(data_file)
 
 for element in data:
-        del element['longitude']
-        del element['latitude']
+        del element['code_region']
+        # del element['longitude']
+        # del element['latitude']
         # del element['code_commune_INSEE']
         # del element['nom_commune_postal']
         # del element['libelle_acheminement']
